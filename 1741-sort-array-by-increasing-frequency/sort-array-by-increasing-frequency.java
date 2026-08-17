7f4925e3f58bdc2e0cbffbd0ecbd77ddfa8001ee
Ajaykumar Nallavelli <ajaykumarnallavelly@gmail.com>
2026-08-17 15:10:41 +0530

    Time: 6 ms (87.42%) | Memory: 45.7 MB (49.28%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -9,16 +9,16 @@ class Solution {
             if(c1 != c2) return c1 - c2; 
             return b.getKey() - a.getKey();
         });
-        int[] res = new int[nums.length];
+        // int[] res = new int[nums.length];
         int j = 0;
         for(Map.Entry<Integer,Integer> e : list){
             int key = e.getKey();
             int freq = e.getValue();
             for(int i=0;i<freq && j<nums.length;i++){
-                res[j] = key;
+                nums[j] = key;
                 j++;
             }
         }
-        return res;
+        return nums;
     }
 }
